ab1d02e58be84c8552b67d964a21b6e596500713
CJP <carlosjavierpacheco60@gmail.com>
2023-08-14 19:04:42 -0400

    src audio

--- src/App.jsx
+++ src/App.jsx
@@ -16,23 +16,23 @@ function App() {
         <div className='pad-container'>
 
           <button className='drum-pad' id='Heater1'>Q
-            <audio src=""></audio></button>
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" className='clip' id='Q'></audio></button>
           <button className='drum-pad' id='Heater2'>W
-            <audio src=""></audio></button>
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" className='clip' id='W'></audio></button>
           <button className='drum-pad' id='Heater3'>E
-            <audio src=""></audio></button>
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" className='clip' id='E'></audio></button>
           <button className='drum-pad' id='Heater4'>A
-            <audio src=""></audio></button>
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" className='clip' id='A'></audio></button>
           <button className='drum-pad' id='Clap'>S
-            <audio src=""></audio></button>
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" className='clip' id='S'></audio></button>
           <button className='drum-pad' id='Open-HH'>D
-            <audio src=""></audio></button>
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" className='clip' id='D'></audio></button>
           <button className='drum-pad' id='kick-n-hat'>Z
-            <audio src=""></audio></button>
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" className='clip' id='Z'></audio></button>
           <button className='drum-pad' id='Kick'>X
-            <audio src=""></audio></button>
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" className='clip' id='X'></audio></button>
           <button className='drum-pad' id='Closed-HH'>C
-            <audio src=""></audio></button>
+            <audio src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" className='clip' id='C'></audio></button>
 
         </div>
 
