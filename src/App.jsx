f4c1d3669ae4303b7f31a812541436d98ac5761d
CJP <carlosjavierpacheco60@gmail.com>
2023-08-15 12:40:56 -0400

    logica

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,62 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
 
-  const [count, setCount] = useState(0)
+  const [display, setdisplay] = useState('')
+
+  const handleChange = ({ target }) => {
+
+    setdisplay(target.id)
+    target.children[0].play()
+
+  }
+
+  const handleKeyDown = (e) => {
+
+    switch (e.key) {
+
+      case 'q': document.getElementById('Heater1').click(); 
+        break
+
+      case 'w': document.getElementById('Heater2').click(); 
+        break
+
+      case 'e': document.getElementById('Heater3').click(); 
+        break
+
+      case 'a': document.getElementById('Heater4').click();
+        break
+
+      case 's': document.getElementById('Clap').click(); 
+        break
+
+      case 'd': document.getElementById('Open-HH').click(); 
+        break
+
+      case 'z': document.getElementById('Kick-n-hat').click(); 
+        break
+
+      case 'x': document.getElementById('Kick').click(); 
+        break
+
+      case 'c': document.getElementById('Closed-HH').click(); 
+        break
+
+      default: break
+
+    }
+
+  }
+
+  useEffect(() => {
+
+
+
+    document.addEventListener('keydown', handleKeyDown)
+
+
+  }, [])
+
 
   return (
 
@@ -15,29 +69,29 @@ function App() {
 
         <div className='pad-container'>
 
-          <button className='drum-pad' id='Heater1'>Q
+          <button className='drum-pad' id='Heater1' onClick={handleChange}>Q
             <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" className='clip' id='Q'></audio></button>
-          <button className='drum-pad' id='Heater2'>W
+          <button className='drum-pad' id='Heater2' onClick={handleChange}>W
             <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" className='clip' id='W'></audio></button>
-          <button className='drum-pad' id='Heater3'>E
+          <button className='drum-pad' id='Heater3' onClick={handleChange}>E
             <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" className='clip' id='E'></audio></button>
-          <button className='drum-pad' id='Heater4'>A
+          <button className='drum-pad' id='Heater4' onClick={handleChange}>A
             <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" className='clip' id='A'></audio></button>
-          <button className='drum-pad' id='Clap'>S
+          <button className='drum-pad' id='Clap' onClick={handleChange}>S
             <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" className='clip' id='S'></audio></button>
-          <button className='drum-pad' id='Open-HH'>D
+          <button className='drum-pad' id='Open-HH' onClick={handleChange}>D
             <audio src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" className='clip' id='D'></audio></button>
-          <button className='drum-pad' id='kick-n-hat'>Z
+          <button className='drum-pad' id='Kick-n-hat' onClick={handleChange}>Z
             <audio src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" className='clip' id='Z'></audio></button>
-          <button className='drum-pad' id='Kick'>X
+          <button className='drum-pad' id='Kick' onClick={handleChange}>X
             <audio src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" className='clip' id='X'></audio></button>
-          <button className='drum-pad' id='Closed-HH'>C
+          <button className='drum-pad' id='Closed-HH' onClick={handleChange}>C
             <audio src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" className='clip' id='C'></audio></button>
 
         </div>
 
         <div className='control'>
-          <p id='display' >algo</p>
+          <p id='display' >{display}</p>
         </div>
 
       </div>
